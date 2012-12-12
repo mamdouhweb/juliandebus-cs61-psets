9b6f7358061b560c44de3e8c2455e62b458c5bdf
Julian Debus <heinzderalligator@googlemail.com>
2012-12-12 11:49:21 -0500

    Sad times

--- pset5/pong61.c
+++ pset5/pong61.c
@@ -81,7 +81,9 @@ typedef struct http_connection {
     size_t content_length;  // Content-Length value
     int has_content_length; // 1 iff Content-Length was provided
 
-    char buf[BUFSIZ];       // Response buffer
+    // Should this be dynamically allocated?
+    //char *buf;
+    char buf[500*BUFSIZ];       // Response buffer
     size_t len;             // Length of response buffer
 } http_connection;
 
@@ -155,6 +157,7 @@ void http_close(http_connection *conn) {
 //    Send an HTTP POST request for `uri` to connection `conn`.
 //    Exit on error.
 void http_send_request(http_connection *conn, const char *uri) {
+    fprintf(stderr, "Connection state: %d\n", conn->state);
     assert(conn->state == HTTP_REQUEST || conn->state == HTTP_DONE);
 
     // prepare and write the request
@@ -214,10 +217,13 @@ void http_receive_response(http_connection *conn) {
             pthread_mutex_unlock(&serverLock);
             pthread_mutex_unlock(&sendLock);
         }
-        ssize_t nr = read(conn->fd, &conn->buf[conn->len], BUFSIZ);
-        
+        int length=500*BUFSIZ;
+        fprintf(stderr,"%zu\n",conn->len);
+        ssize_t nr = read(conn->fd, &conn->buf[conn->len], length);
+        //fprintf(stdout,"%s\n",conn->buf);
+        //fprintf(stderr,"%s\n",conn->buf);
         if(timestamp()-start>0.05)
-            fprintf(stderr,"Delay: %fs\n",(timestamp()-start));
+            fprintf(stderr,"Response was delayed by: %fs\n",(timestamp()-start));
         if (nr == 0)
             eof = 1;
         else if (nr == -1 && errno != EINTR && errno != EAGAIN) {
@@ -225,16 +231,19 @@ void http_receive_response(http_connection *conn) {
             exit(1);
         } else if (nr != -1)
             conn->len += nr;
+        //conn->len=conn->len>BUFSIZ?BUFSIZ:conn->len;
     }
     double wait;
     if(sscanf(conn->buf,"+%lf\n",&wait)==1){
-        fprintf(stderr, "WOO %f\n",wait);
+        fprintf(stderr, "Server begs for wait of: %fs\n",wait);
+        // If reps>1, we have already unlocked sendLock -> reacquire it
         if(reps>1)
             pthread_mutex_lock(&sendLock);
         sleep_for(wait);
         if(reps>1)
             pthread_mutex_unlock(&sendLock);
     }
+    // We need to make sure that conn->len doesn't exceed BUFISZ
     // null-terminate body
     conn->buf[conn->len] = 0;
 
@@ -271,6 +280,7 @@ int slotOfConnection(http_connection *conn) {
     for(int i = 0; i<NCONNECTIONS; ++i){
         if(sharedConnections[i].conn == conn) {
             connection=i;
+            break;
         }
     }
     return connection;
@@ -282,6 +292,7 @@ int connectionSlotWithStatus(con_status searchStatus) {
     for(int i = 0; i<NCONNECTIONS; ++i){
         if(sharedConnections[i].status == searchStatus) {
             connectionToReuse=i;
+            break;
         }
     }
     return connectionToReuse;
@@ -303,6 +314,7 @@ http_connection *getConn(const struct addrinfo *ai) {
     int connectionToReuse = connectionSlotWithStatus(CON_VALID);
     if (connectionToReuse != (-1)) {
         sharedConnections[connectionToReuse].status = CON_INUSE;
+        fprintf(stderr, "Reusing connection %d\n",connectionToReuse);
         return sharedConnections[connectionToReuse].conn;
     }
     
@@ -315,7 +327,7 @@ http_connection *getConn(const struct addrinfo *ai) {
     }
     else {
         // If no connection slot is free -> DOOM AWAITS!
-        assert(1);
+        assert(0);
     }
     return NULL;
 }
@@ -398,9 +410,9 @@ int main(int argc, char **argv) {
         pthread_mutex_unlock(&threadLock);
 
         if(locnthreads<MAXTHREADS){
-            //fprintf(stderr,"Making new thread baby!\n");
             pthread_t thread;
             pthread_create(&thread, NULL, startConnection, &ci);
+            pthread_detach(thread);
             pthread_mutex_lock(&threadLock);
             ++nthreads;
             pthread_mutex_unlock(&threadLock);
@@ -438,13 +450,17 @@ void *startConnection(void *con_info){
     sprintf(url, "move?x=%d&y=%d&style=on", x, y);
 
     do {
+        //pthread_mutex_lock(&threadLock);
         conn = getConn(ci->ai);
+        //pthread_mutex_unlock(&threadLock);
 
         pthread_mutex_lock(&sendLock);
         http_send_request(conn, url);
         http_receive_response(conn);
-        if(conn->status_code==-1){
+        if(conn->status_code==-1) {
+            //pthread_mutex_lock(&threadLock);
             releaseConn(conn);
+            //pthread_mutex_unlock(&threadLock);
             // Exponential backoff
             waittime=waittime==0?MINTIME:2*waittime;
             waittime=MIN(waittime,256*MINTIME);
@@ -580,9 +596,6 @@ static int http_consume_headers(http_connection *conn, int eof) {
             ++i;
     }
     
-//    if(conn->status_code == 200)
-//        pthread_mutex_unlock(&serverLock);
-
     if (conn->state == HTTP_BODY
         && (conn->has_content_length || eof)
         && conn->len >= conn->content_length)
